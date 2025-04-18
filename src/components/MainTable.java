0b097847704e8ac029c1f7f9bed1e388f88cfb72
tnickel2019 <analyser2008@googlemail.com>
2025-04-18 13:11:17 +0200

    add import

--- src/components/MainTable.java
+++ src/components/MainTable.java
@@ -3,6 +3,7 @@ package components;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
